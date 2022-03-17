ae000b8f3373a01fb5d96e417e0d055c138e20a6
Raduchiu <raduchiuamaral@gmail.com>
2022-03-17 15:33:49 -0300

    Creating tabbar, collectionViewCell and a view to upadte the header

---
+++ NetflixClone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UpcomingViewController.swift
+//  NetflixClone
+//
+//  Created by Raduchiu Amaral on 17/03/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    
+}
+   
+
+
